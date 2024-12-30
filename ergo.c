15fc71c4841f814fe02e97442a828c03399e3015
Adam Ridhwan <adamridhwan.1@gmail.com>
2024-12-29 22:49:28 -0500

    update

--- ergo.c
+++ ergo.c
@@ -2,31 +2,29 @@
 
 #ifdef RGBLIGHT_ENABLE
 
-#define WHITE_COLOR RGBLIGHT_LAYER_SEGMENTS({0, 17, HSV_WHITE})
-
-const rgblight_segment_t PROGMEM caps_lock_layer[] = WHITE_COLOR;
-const rgblight_segment_t PROGMEM navigation_layer[] = WHITE_COLOR;
-const rgblight_segment_t PROGMEM number_symbol_layer[] = WHITE_COLOR;
-const rgblight_segment_t PROGMEM function_layer[] = WHITE_COLOR;
+const rgblight_segment_t PROGMEM layer_all[] = RGBLIGHT_LAYER_SEGMENTS({0, 17, HSV_WHITE});
 
 const rgblight_segment_t* const PROGMEM my_rgb_layers[] = RGBLIGHT_LAYERS_LIST(
-    caps_lock_layer,
-    navigation_layer,
-    number_symbol_layer,
-    function_layer
+    layer_all
 );
 
 void keyboard_post_init_kb(void) {
     rgblight_layers = my_rgb_layers;
     keyboard_post_init_user();
-};
+}
 
 bool led_update_kb(led_t led_state) {
     bool res = led_update_user(led_state);
     if (res) {
-        rgblight_set_layer_state(0, led_state.caps_lock);
+        rgblight_set_layer_state(
+            0,
+            layer_state_is(1) ||
+            layer_state_is(2) ||
+            layer_state_is(3) ||
+            led_state.caps_lock
+        );
     }
     return res;
-};
+}
 
 #endif
