29b78dddde20cfa67dc42ecd56d88bfabcd19127
Adam Ridhwan <adamridhwan.1@gmail.com>
2025-01-11 19:38:07 -0500

    update

--- keymaps/adamridhwan/keymap.c
+++ keymaps/adamridhwan/keymap.c
@@ -7,7 +7,6 @@
 bool caps_word_press_user(uint16_t keycode) {
     switch (keycode) {
         case KC_A ... KC_Z:
-        case KC_MINS:
         add_weak_mods(MOD_BIT(KC_LSFT));
             return true;
 
@@ -22,63 +21,6 @@ bool caps_word_press_user(uint16_t keycode) {
     }
 };
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-        case SEARCH:
-            if (record->event.pressed) {
-                tap_code(KC_ESC);
-                register_mods(MOD_BIT(KC_LGUI));
-                tap_code(KC_E);
-                unregister_mods(MOD_BIT(KC_LGUI));
-            }
-            break;
-
-        case HS_A:
-            if (record->tap.count && record->event.pressed) {
-                tap_code16(HYPR(KC_A));
-            } else if (!record->tap.count && record->event.pressed) {
-                register_code(KC_LSFT);
-            } else if (!record->event.pressed) {
-                unregister_code(KC_LSFT);
-            }
-            return false;
-
-        case HC_S:
-            if (record->tap.count && record->event.pressed) {
-                tap_code16(HYPR(KC_S));
-                return false;
-            } else if (!record->tap.count && record->event.pressed) {
-                register_code(KC_LCTL);
-            } else if (!record->event.pressed) {
-                unregister_code(KC_LCTL);
-            }
-            return false;
-
-        case HO_D:
-            if (record->tap.count && record->event.pressed) {
-                tap_code16(HYPR(KC_D));
-                return false;
-            } else if (!record->tap.count && record->event.pressed) {
-                register_code(KC_LALT);
-            } else if (!record->event.pressed) {
-                unregister_code(KC_LALT);
-            }
-            return false;
-
-        case HG_F:
-            if (record->tap.count && record->event.pressed) {
-                tap_code16(HYPR(KC_F));
-                return false;
-            } else if (!record->tap.count && record->event.pressed) {
-                register_code(KC_LGUI);
-            } else if (!record->event.pressed) {
-                unregister_code(KC_LGUI);
-            }
-            return false;
-    }
-    return true;
-}
-
 bool get_permissive_hold(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case SFT_A:
@@ -90,11 +32,6 @@ bool get_permissive_hold(uint16_t keycode, keyrecord_t *record) {
         case CTL_L:
         case SFT_C:
         case NUM_SPC:
-        case HS_A:
-        case HC_S:
-        case HO_D:
-        case HG_F:
-        case HYP_G:
             return true;
         default:
             return false;
@@ -123,11 +60,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //|---------+---------+---------+---------+---------+---------+---------+---------|  |---------+---------+---------+---------+---------+---------+---------+---------|
         XXXXXXX , XXXXXXX ,  HYP_Q  ,  HYP_W  ,  HYP_E  ,  HYP_R  ,  HYP_T  ,              XXXXXXX , KC_HOME ,  KC_UP  ,  KC_END , XXXXXXX , XXXXXXX , XXXXXXX , _______ ,
     //|---------+---------+---------+---------+---------+---------+---------+---------|  |---------+---------+---------+---------+---------+---------+---------+---------|
-        XXXXXXX , CW_TOGG , KC_LSFT , KC_LCTL , KC_LALT , KC_LCMD ,  HYP_G  ,              KC_PGUP , KC_LEFT , KC_DOWN , KC_RGHT , KC_RSFT , XXXXXXX ,      _______      , XXXXXXX ,
+        XXXXXXX , CW_TOGG , KC_LSFT , KC_LCTL , KC_LALT , KC_LCMD ,  HYP_G  ,              KC_PGUP , KC_LEFT , KC_DOWN , KC_RGHT , KC_BSPC , XXXXXXX ,      _______      , XXXXXXX ,
     //|---------+---------+---------+---------+---------+---------+---------+---------|  |---------+---------+---------+---------+---------+---------+---------+---------|
-        XXXXXXX , XXXXXXX ,  HYP_Z  ,  HYP_X  ,  HYP_C  ,  HYP_V  ,  HYP_B  , XXXXXXX ,    KC_PGDN , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,      XXXXXXX      , XXXXXXX ,
+        XXXXXXX , XXXXXXX ,  HYP_Z  ,  HYP_X  ,  HYP_C  ,  HYP_V  ,  HYP_B  , XXXXXXX ,    KC_PGDN , XXXXXXX , XXXXXXX , XXXXXXX , KC_ENT ,      XXXXXXX      , XXXXXXX ,
     //|---------+---------+---------+---------+---------+---------+---------+---------|  |---------+---------+---------+---------+---------+---------+---------+---------|
-                  XXXXXXX , XXXXXXX ,                     XXXXXXX , XXXXXXX ,               KC_SPC , XXXXXXX ,                     XXXXXXX , XXXXXXX , XXXXXXX
+                  XXXXXXX , XXXXXXX ,                     XXXXXXX , XXXXXXX ,              XXXXXXX , XXXXXXX ,                     XXXXXXX , XXXXXXX , XXXXXXX
     //,-------------------------------------------------------------------------------,  ,-------------------------------------------------------------------------------,
     ),
 
@@ -135,13 +72,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //,-------------------------------------------------------------------------------,  ,-------------------------------------------------------------------------------,
         XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,    XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,
     //|---------+---------+---------+---------+---------+---------+---------+---------|  |---------+---------+---------+---------+---------+---------+---------+---------|
-        XXXXXXX , XXXXXXX , XXXXXXX ,  TILDE  ,  LCBRC  ,  RCBRC  ,   PLUS  ,               UNDER  ,   LPRN  ,   RPRN  ,  EQUAL  ,    GT   , XXXXXXX , XXXXXXX , _______ ,
+        XXXXXXX , XXXXXXX ,  GRAVE  ,  TILDE  ,  LCBRC  ,  RCBRC  ,   PLUS  ,               MINUS  ,   LPRN  ,   RPRN  ,  EQUAL  ,    GT   , XXXXXXX , XXXXXXX , _______ ,
     //|---------+---------+---------+---------+---------+---------+---------+---------|  |---------+---------+---------+---------+---------+---------+---------+---------|
-        XXXXXXX , CW_TOGG ,   ONE   ,   TWO   ,  THREE  ,   FOUR  ,   FIVE  ,                SIX   ,  SEVEN  ,  EIGHT  ,   NINE  ,   ZERO  ,  GRAVE  ,      _______      , XXXXXXX ,
+        XXXXXXX , CW_TOGG ,   ONE   ,   TWO   ,  THREE  ,   FOUR  ,   FIVE  ,                SIX   ,  SEVEN  ,  EIGHT  ,   NINE  ,   ZERO  , XXXXXXX ,      _______      , XXXXXXX ,
     //|---------+---------+---------+---------+---------+---------+---------+---------|  |---------+---------+---------+---------+---------+---------+---------+---------|
-        XXXXXXX , XXXXXXX ,  BSLSH  ,   PIPE  ,  LSBRC  ,  RSBRC  ,  MINUS  , XXXXXXX ,    XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,      XXXXXXX      , XXXXXXX ,
+        XXXXXXX , XXXXXXX ,  BSLSH  ,   PIPE  ,  LSBRC  ,  RSBRC  ,  UNDER  , XXXXXXX ,    KC_QUOT , KC_DQUO , XXXXXXX , XXXXXXX , XXXXXXX ,      XXXXXXX      , XXXXXXX ,
     //|---------+---------+---------+---------+---------+---------+---------+---------|  |---------+---------+---------+---------+---------+---------+---------+---------|
-                  XXXXXXX , XXXXXXX ,                     XXXXXXX ,  KC_SPC ,              XXXXXXX , XXXXXXX ,                     XXXXXXX , XXXXXXX , XXXXXXX
+                  XXXXXXX , XXXXXXX ,                     XXXXXXX , XXXXXXX ,              XXXXXXX , XXXXXXX ,                     XXXXXXX , XXXXXXX , XXXXXXX
     //,-------------------------------------------------------------------------------,  ,-------------------------------------------------------------------------------,
     ),
 };
